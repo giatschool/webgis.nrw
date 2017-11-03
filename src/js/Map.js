204be6d95a3a1a2039a031fa1576c5c5f2573f89
felixerdy <felix.erdmann@uni-muenster.de>
2017-11-03 23:04:23 +0100

    population time slider

--- src/js/Map.js
+++ src/js/Map.js
@@ -2,6 +2,7 @@
 import mapboxgl from 'mapbox-gl';
 import MapboxCompare from 'mapbox-gl-compare';
 const KreiseNRW = require('./../data/landkreise_simplify0.json');
+const population = require('./../data/population_data.json');
 const config = require('./../config.js');
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiZmVsaXhhZXRlbSIsImEiOiJjajNicW1lM2QwMDR3MzNwOWdyaXAzN282In0.Pci5KvNNLCjjxy9b4p0n7g';
@@ -11,21 +12,18 @@ var map = new mapboxgl.Map({
     7.8, 51.5
   ],
   zoom: 6,
-  style: (config.theme == 'light' ? 'mapbox://styles/mapbox/light-v9' : 'mapbox://styles/mapbox/dark-v9')
+  style: (
+    config.theme == 'light'
+    ? 'mapbox://styles/mapbox/light-v9'
+    : 'mapbox://styles/mapbox/dark-v9')
 })
 
 map.on('load', () => {
-  console.log(KreiseNRW)
-  // loadData()
-
   // When a click event occurs on a feature in the places layer, open a popup at the
   // location of the feature, with description HTML from its properties.
   map.on('click', 'kreisgrenzen', function(e) {
     if (e.features.length > 0) {
-      new mapboxgl.Popup()
-        .setLngLat(e.lngLat)
-        .setHTML(e.features[0].properties.GEN)
-        .addTo(map);
+      new mapboxgl.Popup().setLngLat(e.lngLat).setHTML(e.features[0].properties.GEN).addTo(map);
     }
   });
 
@@ -41,11 +39,10 @@ map.on('load', () => {
 
   map.on('mousemove', function(e) {
     var states = map.queryRenderedFeatures(e.point, {layers: ['kreisgrenzen']});
-    console.log(states[0])
 
     if (states.length > 0) {
-      // document.getElementById('pd').innerHTML = '<h3><strong>' + states[0].properties.GEN + '</strong></h3><p><strong><em>' + JSON.parse(states[0].properties.destatis).population + '</strong> Einwohner</em></p>';
-      document.getElementById('pd').innerHTML = '<h3><strong>' + states[0].properties.GEN + '</strong></h3>';
+      document.getElementById('pd').innerHTML = '<h3><strong>' + states[0].properties.GEN + '</strong></h3><p><strong><em>' + states[0].properties.population + '</strong> Einwohner</em></p>';
+      // document.getElementById('pd').innerHTML = '<h3><strong>' + states[0].properties.GEN + '</strong></h3>';
     } else {
       document.getElementById('pd').innerHTML = '<p>Hover over a state!</p>';
     }
@@ -54,8 +51,15 @@ map.on('load', () => {
 
 map.on('style.load', () => loadData())
 
-
 function loadData() {
+  KreiseNRW.features.map((kreis) => {
+    population.forEach((kreisPop) => {
+      if (kreis.properties.AGS === kreisPop.AGS) {
+        kreis.properties.population = parseInt(kreisPop.data['1962'])
+      }
+    })
+  })
+
   map.addSource('KreiseNRW', {
     'type': 'geojson',
     'data': KreiseNRW
@@ -65,19 +69,49 @@ function loadData() {
     "type": "fill",
     "source": "KreiseNRW",
     "paint": {
-      "fill-opacity": 0.8
+      "fill-opacity": 0.8,
+      'fill-color': {
+        "property": "population",
+        "stops": [
+          [getMinPop(), '#80BCFF'],
+          [getMaxPop(), '#1A5FAC']
+        ]
+      }
     }
   });
+}
+
+export function updateData(year = '1962') {
+  KreiseNRW.features.map((kreis) => {
+    population.forEach((kreisPop) => {
+      if (kreis.properties.AGS === kreisPop.AGS) {
+        kreis.properties.population = parseInt(kreisPop.data[String(year)])
+      }
+    })
+  })
+  map.getSource('KreiseNRW').setData(KreiseNRW)
+  document.getElementById('year').textContent = year;
+
 }
 
 function getMaxPop() {
   var maxPop = 0;
   KreiseNRW.features.forEach((kreis) => {
-    if (kreis.properties.destatis.population > maxPop) {
-      maxPop = kreis.properties.destatis.population
+    if (kreis.properties.population > maxPop) {
+      maxPop = kreis.properties.population
     }
   })
   return maxPop
+}
+
+function getMinPop() {
+  var minPop = 999999999999;
+  KreiseNRW.features.forEach((kreis) => {
+    if (kreis.properties.population < minPop) {
+      minPop = kreis.properties.population
+    }
+  })
+  return minPop
 }
 
 function getColorValue(val) {
@@ -88,6 +122,5 @@ function getColorValue(val) {
 export function changeStyle() {
   console.log("changing style")
   map.setStyle('mapbox://styles/mapbox/' + config.theme + '-v9');
-
 
 }
