b0f7a08278861eecc36e31cb6c30b37c4d317c53
ubergesundheit <ubergesundheit@googlemail.com>
2017-11-08 19:14:12 +0100

    add prettier and eslint, lint everything

--- src/js/App.js
+++ src/js/App.js
@@ -1,114 +1,145 @@
-import Map from './Map.js'
+import Map from './Map.js';
 
 class App {
   static run() {
-    var map = new Map('map', [7.555,51.478333], 7, (success) => {
-      if(success) {
+    const map = new Map('map', [7.555, 51.478333], 7, success => {
+      if (success) {
         // finished loading
-        document.getElementById('start').removeAttribute('disabled')
-        document.getElementById('start').innerHTML = 'Los geht&#39;s!'
-        document.getElementById('start').setAttribute('data-dismiss', 'modal')
+        document.getElementById('start').removeAttribute('disabled');
+        document.getElementById('start').innerHTML = 'Los geht&#39;s!';
+        document.getElementById('start').setAttribute('data-dismiss', 'modal');
       } else {
-        document.getElementById('start').innerHTML = 'Daten konnten nicht geladen werden :('
+        document.getElementById('start').innerHTML =
+          'Daten konnten nicht geladen werden :(';
         document.getElementById('start').classList.remove('btn-primary');
         document.getElementById('start').classList.add('btn-danger');
       }
-    })
-
-    document.getElementById('basicMap').addEventListener("click", () => {
-      map.changeStyle('basic')
     });
 
-    document.getElementById('darkMap').addEventListener("click", () => {
-      map.changeStyle('dark')
+    document.getElementById('basicMap').addEventListener('click', () => {
+      map.changeStyle('basic');
     });
 
-    document.getElementById('lightMap').addEventListener("click", () => {
-      map.changeStyle('light')
+    document.getElementById('darkMap').addEventListener('click', () => {
+      map.changeStyle('dark');
     });
 
-    document.getElementById('satelliteMap').addEventListener("click", () => {
-      map.changeStyle('satellite')
+    document.getElementById('lightMap').addEventListener('click', () => {
+      map.changeStyle('light');
     });
 
-    document.getElementById('topMap').addEventListener("click", () => {
-      map.changeStyle('top')
+    document.getElementById('satelliteMap').addEventListener('click', () => {
+      map.changeStyle('satellite');
     });
 
-    document.getElementById('dtkMap').addEventListener("click", () => {
-      map.changeStyle('dtk')
+    document.getElementById('topMap').addEventListener('click', () => {
+      map.changeStyle('top');
     });
 
-    document.getElementById('dopMap').addEventListener("click", () => {
-      map.changeStyle('dop')
+    document.getElementById('dtkMap').addEventListener('click', () => {
+      map.changeStyle('dtk');
     });
 
-    document.getElementById('custom_csv_input').addEventListener("change", () => {
-      map.importCSV()
-    })
+    document.getElementById('dopMap').addEventListener('click', () => {
+      map.changeStyle('dop');
+    });
+
+    document
+      .getElementById('custom_csv_input')
+      .addEventListener('change', () => {
+        map.importCSV();
+      });
 
     document.getElementById('slider').addEventListener('input', function(e) {
-        var year = parseInt(e.target.value, 10);
-        map.updateData(year)
+      const year = parseInt(e.target.value, 10);
+      map.updateData(year);
     });
 
-    document.getElementById('transparency-slider').addEventListener('input', function(e) {
-        var transparency = e.target.value;
-        map.changeTransparency(transparency)
+    document
+      .getElementById('transparency-slider')
+      .addEventListener('input', function(e) {
+        const transparency = e.target.value;
+        map.changeTransparency(transparency);
+      });
+
+    document
+      .getElementsByName('population_data')[0]
+      .addEventListener('click', () => {
+        map.setData('population_data', 'population');
+      });
+
+    document
+      .getElementById('Anteil_Arbeitslose_UTF8')
+      .addEventListener('click', () => {
+        map.setData('Anteil_Arbeitslose_UTF8', 'arbeitslose');
+      });
+
+    document
+      .getElementById('Erwerbstaetige_Dienstleistung')
+      .addEventListener('click', () => {
+        map.setData(
+          'Anteil_Erwerbstaetige_Dienstleistung_UTF8',
+          'Erwerbstaetige_Dienstleistung'
+        );
+      });
+
+    document
+      .getElementById('Erwerbstaetige_Forst')
+      .addEventListener('click', () => {
+        map.setData('Anteil_Erwerbstaetige_Forst_UTF8', 'Erwerbstaetige_Forst');
+      });
+
+    document
+      .getElementById('Erwerbstaetige_Gewerbe')
+      .addEventListener('click', () => {
+        map.setData(
+          'Anteil_Erwerbstaetige_ProduzierendesGewerbe_UTF8',
+          'Erwerbstaetige_Gewerbe'
+        );
+      });
+
+    document.getElementById('Wahl17_CDU').addEventListener('click', () => {
+      map.setData('Wahlergebnisse_CDU_1976_bis_2013', 'Wahl17_CDU');
     });
 
-    document.getElementsByName('population_data')[0].addEventListener("click", () => {
-      map.setData('population_data', 'population')
+    document.getElementById('Wahl17_SPD').addEventListener('click', () => {
+      map.setData('Wahlergebnisse_CDU_1976_bis_2013', 'Wahl17_SPD');
     });
 
-    document.getElementById('Anteil_Arbeitslose_UTF8').addEventListener("click", () => {
-      map.setData('Anteil_Arbeitslose_UTF8', 'arbeitslose')
+    document.getElementById('feinstaub01').addEventListener('click', () => {
+      map.addFeinstaubLayer('band01_02112017');
     });
 
-    document.getElementById('Erwerbstaetige_Dienstleistung').addEventListener("click", () => {
-      map.setData('Anteil_Erwerbstaetige_Dienstleistung_UTF8', 'Erwerbstaetige_Dienstleistung')
+    document.getElementById('feinstaub12').addEventListener('click', () => {
+      map.addFeinstaubLayer('band12_02112017');
     });
 
-    document.getElementById('Erwerbstaetige_Forst').addEventListener("click", () => {
-      map.setData('Anteil_Erwerbstaetige_Forst_UTF8', 'Erwerbstaetige_Forst')
+    document.getElementById('feinstaub24').addEventListener('click', () => {
+      map.addFeinstaubLayer('band24_02112017');
     });
 
-    document.getElementById('Erwerbstaetige_Gewerbe').addEventListener("click", () => {
-      map.setData('Anteil_Erwerbstaetige_ProduzierendesGewerbe_UTF8', 'Erwerbstaetige_Gewerbe')
-    });
+    document
+      .getElementById('feinstaub-remove')
+      .addEventListener('click', () => {
+        map.removeFeinstaubLayer();
+      });
 
-    document.getElementById('Wahl17_CDU').addEventListener("click", () => {
-      map.setData('Wahlergebnisse_CDU_1976_bis_2013', 'Wahl17_CDU')
-    });
+    document.getElementById('lowColor').addEventListener(
+      'change',
+      e => {
+        map.changeColor('low', e.target.value);
+      },
+      true
+    );
 
-    document.getElementById('Wahl17_SPD').addEventListener("click", () => {
-      map.setData('Wahlergebnisse_CDU_1976_bis_2013', 'Wahl17_SPD')
-    });
-
-    document.getElementById('feinstaub01').addEventListener("click", () => {
-      map.addFeinstaubLayer('band01_02112017')
-    });
-
-    document.getElementById('feinstaub12').addEventListener("click", () => {
-      map.addFeinstaubLayer('band12_02112017')
-    });
-
-    document.getElementById('feinstaub24').addEventListener("click", () => {
-      map.addFeinstaubLayer('band24_02112017')
-    });
-
-    document.getElementById('feinstaub-remove').addEventListener("click", () => {
-      map.removeFeinstaubLayer()
-    });
-
-    document.getElementById('lowColor').addEventListener("change", (e) => {
-      map.changeColor('low', e.target.value)
-    },  true);
-
-    document.getElementById('highColor').addEventListener("change", (e) => {
-      map.changeColor('high', e.target.value)
-    }, true);
+    document.getElementById('highColor').addEventListener(
+      'change',
+      e => {
+        map.changeColor('high', e.target.value);
+      },
+      true
+    );
   }
 }
 
-export default App
+export default App;
